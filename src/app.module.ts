7212c55e45c9f81b65a418d0314a96040c59f5a1
Eugenio Blanco <eblancofe@uoc.edu>
2026-05-17 12:14:58 +0200

    Fix DB_PORT parseInt

--- src/app.module.ts
+++ src/app.module.ts
@@ -39,6 +39,7 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
+    /*
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
@@ -48,6 +49,20 @@ import { AuthModule } from './auth/auth.module';
       database: 'db_expedientes',
       autoLoadEntities: true,
       synchronize: false,
+    }),
+    */
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '5432'),
+      username: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASS || 'KpT5Fi2Dr3bJ',
+      database: process.env.DB_NAME || 'db_expedientes',
+      autoLoadEntities: true,
+      synchronize: false,
+      ssl: process.env.NODE_ENV === 'production'
+        ? { rejectUnauthorized: false }
+        : false,
     }),
     UsersModule,
     ExpertosModule,
